c9a8bede280c16e5c725885e2dc4f77b604a86b6
bek1F <xamidullayevbehruz1@gmail.com>
2025-10-07 16:20:52 +0500

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,8 @@
+alert(`hozir sizda random sonlar chiqishni boshlidi`);
+ let son = Math.floor(Math.random()*100);
+ let natija = son * 2;  
+ let natija2 = natija / 4;
+ 
+alert(`sizning  random sonongiz  ${son}`);
+alert(`random sonongizni 2ga ko'paytirdim`);
+alert(`randoim chiqgan son ${son} ni 2ga ko'paytirdim va natija ${natija} chiqdi va chiqgan sonni 4ga bo'ldim natija ${natija2} chiqdi`);
